50c546fff896bad510bae59ca24547c2f0624a95
Roman <roman.blyk@gmail.com>
2020-09-14 12:52:27 +0300

    work with server php

--- dist/js/script.js
+++ dist/js/script.js
@@ -87,23 +87,23 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Modal
 
-    const   modal = document.querySelector('.modal'),
-            dataModal = document.querySelectorAll('[data-modal]'),
-            closeModal = document.querySelector('[modal-close]');
+    const modal = document.querySelector('.modal'),
+        dataModal = document.querySelectorAll('[data-modal]'),
+        closeModal = document.querySelector('[modal-close]');
 
     function showModalWindow() {
         modal.classList.add('show');
         modal.classList.remove('hide');
         document.body.style.overflow = 'hidden';
-        clearInterval(modalTimerId);
+        // clearInterval(modalTimerId);
     }
 
-    
+
     function closeModalFoo() {
         modal.classList.add('hide');
         modal.classList.remove('show');
         document.body.style.overflow = '';
-        
+
     }
 
     dataModal.forEach(e => {
@@ -117,22 +117,141 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
     document.addEventListener('keydown', (btn) => {
-        if(btn.code === "Escape" && modal.classList.contains('show')) {
+        if (btn.code === "Escape" && modal.classList.contains('show')) {
             closeModalFoo();
         }
     });
 
-    const modalTimerId = setTimeout(showModalWindow, 3000);
+    // const modalTimerId = setTimeout(showModalWindow, 5000);
 
     function showModalByScroll() {
-        if(document.documentElement.clientHeight + window.pageYOffset >= document.documentElement.scrollHeight) {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
             showModalWindow();
             window.removeEventListener('scroll', showModalByScroll);
         }
     }
 
     window.addEventListener('scroll', showModalByScroll);
+
+
+    class Menu {
+        constructor(src, alt, title, dsc, price, parentSelector, ...classes) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.dsc = dsc;
+            this.price = price;
+            this.classes = classes;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.chengeToUAH();
+
+        }
+
+        chengeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+        render() {
+            const element = document.createElement('div');
+
+            if (this.classes.length === 0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+
+            element.innerHTML = 
+                    `<img src=${this.src} alt=${this.alt}>
+                     <h3 class="menu__item-subtitle">${this.title}</h3>
+                     <div class="menu__item-descr">${this.dsc}</div> 
+                     <div class="menu__item-divider"></div>
+                     <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div> 
+                        <div class="menu__item-total"><span>${this.price}</span>грн/день</div> 
+                     </div>`;
+            this.parent.append(element);
+        }
+
+    }
+    new Menu(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container',
+        
+    ).render();
+    new Menu(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню "Премиум"',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        19,
+        '.menu .container',
         
 
+    ).render();
+    new Menu(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        14,
+        '.menu .container',
+        
 
+    ).render();
+
+    // Forms
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: "Загрузка",
+        success: "Спасибо! Скоро мы свами свяжемся",
+        failure: "Что-то пошло не так..."
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const rex = new XMLHttpRequest();
+            rex.open('POST', "server.php");
+
+            rex.setRequestHeader('Content-type', 'application/json');
+            const formData = new FormData(form);
+
+            const object = {};
+            form.forEach(function(value, key) {
+                object[key] = value;
+            });
+
+            const json = JSON.stringify(object);
+
+            rex.send(json);
+            rex.addEventListener('load', () => {
+                if (rex.status === 200) {
+                    console.log(rex.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
+                }else{
+                    statusMessage.textContent = message.failure;
+                }
+            });
+        });
+    }
 });
